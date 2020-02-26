cefdeac3fdc8ee275965476ca066f21c7903265c
asbyvshev <barseto4ka@live.ru>
2020-02-26 16:08:04 +0300

    созданы методы удаления\добавления продуката\покупателя, набросок метода поиска - не риализован

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,65 +5,83 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
-            SessionFactory factory = new Configuration()
+        SessionFactory factory = new Configuration()
                     .configure("hibernate.cfg.xml")
                     .addAnnotatedClass(Customer.class)
                     .addAnnotatedClass(Product.class)
 //                    .addAnnotatedClass(ShopCart.class)
                     .buildSessionFactory();
-            // CRUD
-            Session session = null;
 
-        // READ
-//
-//        try {
-////           // CREATE
-//            session = factory.getCurrentSession();
-//            Customer tmpCust = new Customer();
-//            tmpCust.setName("Pupkin");
-//            session.beginTransaction();
-//            session.save(tmpCust);
-//            session.getTransaction().commit();
-//        } finally {
-//            factory.close();
-//            session.close();
-//        }
-//        try {
-////           // CREATE
-//            session = factory.getCurrentSession();
-//            Product tmpProd = new Product();
-//            tmpProd.setTitle("Audio sistem");
-//            tmpProd.setCost(1.22);
-//            session.beginTransaction();
-//            session.save(tmpProd);
-//            session.getTransaction().commit();
-//        } finally {
-//            factory.close();
-//            session.close();
-//        }
+           delCustomerById(factory,1);
 
-//        try {
-////           // READ
-//            session = factory.getCurrentSession();
-//            session.beginTransaction();
-//            Product product = session.get(Product.class,2);
-//            System.out.println(product);
-//            session.getTransaction().commit();
-//        } finally {
-//            factory.close();
-//            session.close();
-//        }
+    }
+    public static void insProduct(SessionFactory factory, String title, double cost){
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            Product tmpProd = new Product();
+            tmpProd.setTitle(title);
+            tmpProd.setCost(cost);
+            session.beginTransaction();
+            session.save(tmpProd);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+            session.close();
+        }
+    }
+    public static void insCustomer(SessionFactory factory, String name){
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            Customer tmpCust = new Customer();
+            tmpCust.setName(name);
+            session.beginTransaction();
+            session.save(tmpCust);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+            session.close();
+        }
+    }
 
-//        try {
-//////           // DELETE
-//            session = factory.getCurrentSession();
-//            session.beginTransaction();
-//            Product product = session.get(Product.class,4);
-//            session.delete(product);
-//            session.getTransaction().commit();
-//        } finally {
-//            factory.close();
-//            session.close();
-//        }
+    public static void delCustomerById(SessionFactory factory, int id){
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Customer customer = session.get(Customer.class,id);
+            session.delete(customer);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+            session.close();
+        }
+    }
+    public static void delProductById(SessionFactory factory, int id){
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Product product = session.get(Product.class,id);
+            session.delete(product);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+            session.close();
+        }
+    }
+    public static void findAllBuyProductByTitle (SessionFactory factory, String prodTitle){
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Product product = session.get(Product.class,2);
+            System.out.println(product);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+            session.close();
+        }
     }
 }
