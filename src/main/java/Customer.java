309b6d1ab986c86cb6d1efd65ab8d0f23b8780ae
asbyvshev <barseto4ka@live.ru>
2020-02-26 12:25:40 +0300

    реализация классов без аннотаций

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,32 @@
+import java.util.List;
+
 public class Customer {
+
+    private int id;
+    private String name;
+    private List <Product> products;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,40 @@
+import java.util.List;
+
 public class Product {
+    private int id;
+    private String title;
+    private double coust;
+    private List<Customer> customers;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCoust() {
+        return coust;
+    }
+
+    public void setCoust(double coust) {
+        this.coust = coust;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
 }
